52a0802b7d6a70e3fd7cd274c03dcb94559c6d86
Vyary <vyaryw@gmail.com>
2024-12-16 00:58:30 +0000

    route change

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -9,7 +9,7 @@ func (s *Server) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.Handle("/", http.NotFoundHandler())
-	mux.Handle("GET /api/v1/poe2/uniques", middleware.CacheControl(s.GetUniques2()))
+	mux.Handle("GET /api/v1/uniques2", middleware.CacheControl(s.GetUniques2()))
 
 	return mux
 }
