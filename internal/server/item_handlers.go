38ae7124f8fba0a616d6a63f269477b7e633dba2
Vyary <vyaryw@gmail.com>
2024-12-31 01:52:19 +0000

    add fragments and ui update

--- internal/server/item_handlers.go
+++ internal/server/item_handlers.go
@@ -40,3 +40,15 @@ func (s *Server) GetCores2() http.Handler {
 		utils.Encode(w, r, u, true)
 	})
 }
+
+func (s *Server) GetFragments2() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		u, err := s.db.GetExch("fragments2")
+		if err != nil {
+			utils.Error(w, http.StatusInternalServerError, "An internal server error occurred. Please try again later.", err, nil)
+			return
+		}
+
+		utils.Encode(w, r, u, true)
+	})
+}
