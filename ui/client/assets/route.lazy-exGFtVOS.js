c9b13a7508295005d5fc8ea7af56dc8b9163766f
Vyary <vyaryw@gmail.com>
2025-01-09 00:24:05 +0000

    UI - background image optimization

---
+++ ui/client/assets/route.lazy-exGFtVOS.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,D as a,e as s}from"./index-CCJgaVU7.js";const n=e("/cores")({component:c});function c(){const o=n.useLoaderData();return t.jsx(a,{columns:s,data:o})}export{n as Route};
