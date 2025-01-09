fc87f7686a90f6dc7c5f9eb4269f78d590d27b69
Vyary <vyaryw@gmail.com>
2025-01-09 15:07:50 +0000

    remote db connection fix

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,9 +1,20 @@
 package database
 
-import "vault/internal/models"
+import (
+	"database/sql"
+	"vault/internal/models"
+)
 
 type Client interface {
 	GetUniques2() ([]models.UniquesDTO, error)
 	GetExch(tableName string) ([]models.ExchDTO, error)
-  Health() error
+	Health() error
+}
+
+type LibsqlClient struct {
+	DB *sql.DB
+}
+
+func (l *LibsqlClient) Health() error {
+  return l.DB.Ping()
 }
