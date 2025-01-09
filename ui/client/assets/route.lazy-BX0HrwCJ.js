7b3e11ed704f0be8b3a78631364b4334f1be448a
Vyary <vyaryw@gmail.com>
2025-01-09 00:10:07 +0000

    UI - home page update

---
+++ ui/client/assets/route.lazy-BX0HrwCJ.js
@@ -0,0 +1,1 @@
+import{c as e,j as t,D as a,e as s}from"./index-D20Rj7Yc.js";const n=e("/cores")({component:c});function c(){const o=n.useLoaderData();return t.jsx(a,{columns:s,data:o})}export{n as Route};
